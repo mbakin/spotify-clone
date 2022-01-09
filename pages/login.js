e500d3047c57f4d94c7a819002c4eaa1e428d524
mbakin <mehmetberkakinn@outlook.com>
2022-01-09 23:47:21 +0300

    feat: create login function and settings spotify api config

--- pages/login.js
+++ pages/login.js
@@ -1,21 +1,35 @@
 import { getProviders, signIn } from "next-auth/react";
-
-function login() {
+import Image from "next/image";
+function login({ providers }) {
   return (
-    <div>
-      login page?
+    <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
+      {" "}
+      <Image
+        className="w-52 ml-10"
+        src="https://links.papareact.com/9xl"
+        alt="brand-logo"
+        width={208}
+        height={208}
+      />{" "}
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          {" "}
+          <button
+            className="bg-[#18D860] text-white p-5 mt-6 rounded-full"
+            onClick={() => {
+              signIn(provider.id, { callbackUrl: "/" });
+            }}
+          >
+            {" "}
+            Login with <b>{provider.name}</b>{" "}
+          </button>{" "}
+        </div>
+      ))}{" "}
     </div>
   );
 }
-
 export default login;
-
 export async function getServerSideProps() {
   const providers = await getProviders();
-
-  return {
-    props: {
-      providers,
-    },
-  };
+  return { props: { providers } };
 }
