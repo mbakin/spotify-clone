c225f6d791d9372b9447f5ab709a1e9d75f6cfec
mbakin <mehmetberkakinn@outlook.com>
2022-01-09 23:44:30 +0300

    fix: variables naming and next.config.js domain url

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -31,8 +31,8 @@ export default NextAuth({
 	// Configure one or more authentication providers
 	providers: [
 		SpotifyProvider({
-			clientId: process.env.SPOTIFY_CLIENT_ID,
-			clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+			clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+			clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
 			authorization: LOGIN_URL,
 		}),
 	],
--- pages/login.js
+++ pages/login.js
@@ -1,21 +1,21 @@
-import { getProviders, signIn } from 'next-auth/react';ß
+import { getProviders, signIn } from "next-auth/react";
 
 function login() {
   return (
     <div>
       login page?
     </div>
-  )
+  );
 }
 
-export default login
+export default login;
 
 export async function getServerSideProps() {
-  const providers = await getProviders()
+  const providers = await getProviders();
 
-  return{
+  return {
     props: {
       providers,
-    }
-  }
+    },
+  };
 }
