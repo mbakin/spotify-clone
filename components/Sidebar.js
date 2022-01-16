410e2d82e0bd1ec7ba1e403fab4501bd188bd534
mbakin <mehmetberkakinn@outlook.com>
2022-01-16 15:21:01 +0300

    WIP: fetch users playlist in SpotifyWebApi service

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -8,10 +8,25 @@ import {
   LogoutIcon,
 } from "@heroicons/react/outline";
 import { signOut, useSession } from "next-auth/react";
+import { useState, useEffect } from "react";
+import useSpotify from "../hook/useSpotify";
+
 
 function Sidebar() {
-
+  const spotifyApi = useSpotify();
   const { data : session, status } = useSession();
+  const [ playlists, setPlaylists ] = useState([]); 
+
+  useEffect(() => {
+    if (spotifyApi.getAccessToken()) {
+      spotifyApi.getUserPlaylists().then((data) => {
+        setPlaylists(data.body.items);
+      });
+      
+    }
+  }, [session, spotifyApi ]);
+
+  console.log(playlists);
 
   return (
     <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll h-screen scrollbar-hide">
