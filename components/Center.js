294e248392bdd57b984a66ebeccfd5e26c519803
mbakin <mehmetberkakinn@outlook.com>
2022-01-17 19:59:30 +0300

    fix: change to `useRecoilValue` from `useRecoilState` for read only

--- components/Center.js
+++ components/Center.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { ChevronDownIcon } from "@heroicons/react/outline";
 import { shuffle } from "lodash";
-import { useRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import { playlistIdState } from "../atoms/playlistAtom";
 
 const colors = [
@@ -19,7 +19,7 @@ const colors = [
 function Center() {
   const { data: session } = useSession();
   const [color, setColor] = useState(null);
-  const [playlistId, setPlaylistId] = useRecoilState(playlistIdState);
+  const playlistId = useRecoilValue(playlistIdState);
 
   useEffect(() => {
     setColor(shuffle(colors)[0]);
