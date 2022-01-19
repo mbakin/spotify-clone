72c9f21d4b8ecfade6fe4dbb8b4489290190686f
mbakin <mehmetberkakinn@outlook.com>
2022-01-19 21:36:14 +0300

    fix: GET request response 401 error fixed on console

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Sidebar from '../components/Sidebar'
 import Center from '../components/Center'
 import styles from '../styles/Home.module.css'
+import { getSession } from 'next-auth/react'
 
 export default function Home() {
   return (
@@ -14,4 +15,14 @@ export default function Home() {
     
     </div>
   )
+}
+
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  return {
+    props: {
+      session,
+    }
+  }
 }
