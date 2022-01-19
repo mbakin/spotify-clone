a6acb7cc833fcda8bbc8e6015583af3ccc7142db
mbakin <mehmetberkakinn@outlook.com>
2022-01-19 23:38:30 +0300

    feat: implement song state information and session info

--- components/Player.js
+++ components/Player.js
@@ -1,7 +1,22 @@
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+import useSpotify from "../hook/useSpotify"
+import { useRecoilState } from 'recoil';
+import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
+
 function Player() {
+
+  const spotifyApi = useSpotify();
+  const { data: session, status} = useSession();
+  const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState);
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+  const [volume, setVolume] = useState(40);
+
   return (
-    <div>
-      Player components
+    <div >
+      <div>
+        <img src="" alt="" />
+      </div>
     </div>
   )
 }
