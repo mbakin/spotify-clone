70586c71bfeca5df26dfc87930a862ace74e8be4
mbakin <mehmetberkakinn@outlook.com>
2022-01-19 22:58:20 +0300

    feat: `signOut` function transferred to `Center.js` component

--- components/Center.js
+++ components/Center.js
@@ -6,6 +6,7 @@ import { useRecoilState, useRecoilValue } from "recoil";
 import { playlistIdState, playlistState } from "../atoms/playlistAtom";
 import useSpotify from '../hook/useSpotify';
 import Songs from "./Songs";
+import { signOut } from "next-auth/react";
 
 const colors = [
   "from-indigo-500",
@@ -41,7 +42,7 @@ function Center() {
   return (
     <div className="flex-grow h-screen overflow-x-scroll scrollbar-hide">
       <header className="absolute top-5 right-8">
-        <div className="flex items-center bg-black text-white space-x-3 p-1 pr-2  opacity-90 hover:opacity-80 cursor-pointer rounded-full">
+        <div className="flex items-center bg-black text-white space-x-3 p-1 pr-2  opacity-90 hover:opacity-80 cursor-pointer rounded-full" onClick={signOut}>
           <img className="rounded-full w-10 h-10" src={session?.user.image} url="user-avatar" />
           <h2>{session?.user.name}</h2>
           <ChevronDownIcon className="w-5 h-5" />
