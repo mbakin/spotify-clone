2e3e35ac6d3bd91c91aa6e6b498ae0d93afffde2
mbakin <mehmetberkakinn@outlook.com>
2022-01-19 21:53:12 +0300

    feat: create `Songs` component and return playlist songs

--- components/Songs.js
+++ components/Songs.js
@@ -1,7 +1,15 @@
+import { useRecoilValue } from 'recoil';
+import { playlistState } from '../atoms/playlistAtom';
+
 function Songs() {
+
+  const playlist = useRecoilValue(playlistState);
+
   return (
-    <div>
-      Songs components
+    <div className='text-white'>
+      {playlist?.tracks.items.map((track) => (
+        <div key={track.id}>{track.track.name}</div>
+      ))}
     </div>
   )
 }
