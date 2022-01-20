1d2ea0a689d929a0b6c80de073d0bd2a3ce20084
mbakin <mehmetberkakinn@outlook.com>
2022-01-20 11:42:22 +0300

    fix: update custom hook `then` callback

--- hook/useSongInfo.js
+++ hook/useSongInfo.js
@@ -1,27 +1,29 @@
 import { useRecoilState } from "recoil";
 import { currentIdTrackState } from "../atoms/songAtom";
 import useSpotify from "./useSpotify";
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 function useSongInfo() {
-
   const spotifyApi = useSpotify();
-  const [currentIdTrack, setCurrentIdTrack] = useRecoilState(currentIdTrackState);
+  const [currentIdTrack, setCurrentIdTrack] =
+    useRecoilState(currentIdTrackState);
   const [songInfo, setSongInfo] = useState(null);
 
   useEffect(() => {
     const fetchSongInfo = async () => {
-      if( currentIdTrack ){
+      if (currentIdTrack) {
         const trackInfo = await fetch(
           `https://api.spotify.com/v1/tracks/${currentIdTrack}`,
           {
             headers: {
               Authorization: `Bearer ${spotifyApi.getAccessToken()}`,
-            }
+            },
           }
-        )
+        ).then((res) => res.json());
+
+        setSongInfo(trackInfo);
       }
-    }
+    };
 
     fetchSongInfo();
   }, [currentIdTrack, spotifyApi]);
