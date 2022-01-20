63bc78797e0e5ba28f05db9527cb67601f6e8b51
mbakin <mehmetberkakinn@outlook.com>
2022-01-20 11:55:51 +0300

    feat: add a image for `Player` component

--- components/Player.js
+++ components/Player.js
@@ -1,24 +1,27 @@
 import { useSession } from "next-auth/react";
 import { useState } from "react";
-import useSpotify from "../hook/useSpotify"
-import { useRecoilState } from 'recoil';
+import useSpotify from "../hook/useSpotify";
+import { useRecoilState } from "recoil";
 import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
+import useSongInfo from "../hook/useSongInfo";
 
 function Player() {
-
   const spotifyApi = useSpotify();
-  const { data: session, status} = useSession();
-  const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState);
+  const { data: session, status } = useSession();
+  const [currentTrackId, setCurrentTrackId] =
+    useRecoilState(currentTrackIdState);
   const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
   const [volume, setVolume] = useState(40);
 
+  const songInfo = useSongInfo();
+
   return (
-    <div >
+    <div>
       <div>
-        <img src="" alt="" />
+        <img className="hidden md:inline h-10 w-10" src={songInfo?.album.images?.[0]?.url} alt="" />
       </div>
     </div>
-  )
+  );
 }
 
-export default Player
+export default Player;
