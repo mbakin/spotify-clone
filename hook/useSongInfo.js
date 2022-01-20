24c1535371b014e4bdec205b248ca231de51472e
mbakin <mehmetberkakinn@outlook.com>
2022-01-20 11:40:01 +0300

    feat: new custom hook `songInfo` used Player component

---
+++ hook/useSongInfo.js
@@ -0,0 +1,32 @@
+import { useRecoilState } from "recoil";
+import { currentIdTrackState } from "../atoms/songAtom";
+import useSpotify from "./useSpotify";
+import { useEffect } from 'react';
+
+function useSongInfo() {
+
+  const spotifyApi = useSpotify();
+  const [currentIdTrack, setCurrentIdTrack] = useRecoilState(currentIdTrackState);
+  const [songInfo, setSongInfo] = useState(null);
+
+  useEffect(() => {
+    const fetchSongInfo = async () => {
+      if( currentIdTrack ){
+        const trackInfo = await fetch(
+          `https://api.spotify.com/v1/tracks/${currentIdTrack}`,
+          {
+            headers: {
+              Authorization: `Bearer ${spotifyApi.getAccessToken()}`,
+            }
+          }
+        )
+      }
+    }
+
+    fetchSongInfo();
+  }, [currentIdTrack, spotifyApi]);
+
+  return songInfo;
+}
+
+export default useSongInfo;
