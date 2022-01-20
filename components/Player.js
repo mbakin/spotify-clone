3f0dbd0b759afdda44322a22ea05dc7116b82fdd
mbakin <mehmetberkakinn@outlook.com>
2022-01-20 12:29:03 +0300

    style: feat new style class for `Player` component

--- components/Player.js
+++ components/Player.js
@@ -36,8 +36,8 @@ function Player() {
   }, [currentTrackIdState, spotifyApi, session])
 
   return (
-    <div>
-      <div>
+    <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
+      <div className="flex items-center space-x-4">
         <img className="hidden md:inline h-10 w-10" src={songInfo?.album.images?.[0]?.url} alt="" />
         <div>
           <h3>{songInfo?.name}</h3>
