499ac90148d17d2b2cb609b70191f0b10db31675
mbakin <mehmetberkakinn@outlook.com>
2022-01-20 12:20:05 +0300

    chore: change default playlist id

--- atoms/playlistAtom.js
+++ atoms/playlistAtom.js
@@ -7,5 +7,5 @@ export const playlistState = atom({
 
 export const playlistIdState = atom({
   key: 'playlistIdState',
-  default : '7uNMaEENBSW9jJQLftNkfE',
+  default : '5IYybZWavhd6mELEPfLwwb',
 });
