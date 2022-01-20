941f69ab9b8566d778f206a054a9bf9df5ad5bcf
mbakin <mehmetberkakinn@outlook.com>
2022-01-20 13:26:48 +0300

    feat: create icons function volumeUp and volumeDown

--- components/Player.js
+++ components/Player.js
@@ -49,9 +49,8 @@ function Player() {
         spotifyApi.play();
         setIsPlaying(true);
       }
-    })
-
-  }
+    });
+  };
 
   useEffect(() => {
     if (spotifyApi.getAccessToken() && !currentTrackId) {
@@ -86,6 +85,19 @@ function Player() {
 
         <FastForwardIcon className="button" />
         <ReplyIcon className="button" />
+      </div>
+
+      <div className="flex items-center space-x-3 md:space-x-4 justify-end pr-5">
+        <VolumeDownIcon onClick={() => volume > 0 && setVolume(volume -10)} className="button" />
+        <input
+          className="w-14 md:w-28"
+          type="range"
+          value={volume}
+          onChange={(e) => setVolume(Number(e.target.value))}
+          min={0}
+          max={100}
+        />
+        <VolumeUpIcon onClick={() => volume < 100 && setVolume(volume + 10)} className="button" />
       </div>
     </div>
   );
