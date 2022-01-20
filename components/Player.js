f7dfc313fb6d8ee187d9207ebbbb86bf800857a5
mbakin <mehmetberkakinn@outlook.com>
2022-01-20 12:18:59 +0300

    feat: update a song name and artist name

--- components/Player.js
+++ components/Player.js
@@ -1,5 +1,5 @@
 import { useSession } from "next-auth/react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import useSpotify from "../hook/useSpotify";
 import { useRecoilState } from "recoil";
 import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
@@ -15,10 +15,34 @@ function Player() {
 
   const songInfo = useSongInfo();
 
+  const fetchCurrentSong = () => {
+    if (!songInfo) {
+      spotifyApi.getMyCurrentPlayingTrack().then(data => {
+        setCurrentTrackId(data.body?.item?.id);
+
+        spotifyApi.getMyCurrentPlaybackState().then(data => {
+          setIsPlaying(data.body?.is_playing);
+        });
+      });
+    }
+  }
+
+  useEffect(() => {
+    if(spotifyApi.getAccessToken() && !currentTrackId)
+    {
+      fetchCurrentSong();
+      setVolume(40);
+    }
+  }, [currentTrackIdState, spotifyApi, session])
+
   return (
     <div>
       <div>
         <img className="hidden md:inline h-10 w-10" src={songInfo?.album.images?.[0]?.url} alt="" />
+        <div>
+          <h3>{songInfo?.name}</h3>
+          <p>{songInfo?.artists?.[0]?.name}</p>
+        </div>
       </div>
     </div>
   );
